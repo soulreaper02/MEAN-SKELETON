91a3077dca0ba956b7e5bc87876142e52038aa47
Amit Kumar <slayer9191@gmail.com>
2018-04-11 22:10:47 +0530

    Update app.js

--- routes/app.js
+++ routes/app.js
@@ -4,5 +4,9 @@ var router = express.Router();
 router.get('/', function (req, res, next) {
     res.render('index');
 });
+router.post('/', function (req, res, next) {
+    var message = new Message({
+        content: req.body.content
+    });
 
 module.exports = router;
